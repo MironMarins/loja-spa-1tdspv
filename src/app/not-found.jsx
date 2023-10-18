85aa9fbb72975fdfb4fa976389e47d778a97f23e
MironMarins <rm551801@fiap.co.br>
2023-10-17 23:01:03 -0300

    muito mais coisas ainda calças a consertar

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -6,10 +6,10 @@ export default function NotFound() {
         <h1>OOPS! 400</h1>
         <p>not found</p>
         <div>
-            <Image src=""
-            alt=""
+            {<Image src="https://httpstatusdogs.com/404-not-found"
+            alt="Cachorro cavando"
             width={500}
-            feight={500}>
+            feight={500}/>}
                            
         </div>
 
