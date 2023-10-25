2867f62646a391c476a256a23153aaa88f932d17
MironMarins <rm551801@fiap.com.br>
2023-10-24 22:12:18 -0300

    teste

---
+++ src/app/api/git-users/route.jsx
@@ -0,0 +1,7 @@
+export async function GET(request,{params}) {
+
+    const response = await fetch("https://api.github.com/users");
+    const users = await response.json();
+    
+    return NextResponse.json(users);
+}
